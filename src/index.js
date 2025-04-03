fcfc1c094e5215164c1fceb50cb1696ab5b2d567
jade <121040221+stephenstrange-blip@users.noreply.github.com>
2025-04-03 15:20:02 +0800

    Chore: Change text

--- src/index.js
+++ src/index.js
@@ -7,27 +7,29 @@ import linkedinIcon from './assets/linkedin.svg';
 
 
 (function addText() {
-  const name = 'Jade Isip';
-  const aboutMe = 'About Me';
-  const aboutInfo = 'I am an aspiring software engineer from the Land of the Pearl of the Orients. My end goal is to become a cloud engineer but I also took interest in web development, hence why I am in the TOD curriculum. If you want to find a coding buddy or you need someone for backend development, primarily something to do with the cloud, then do not hesitate to reach out to me!';
+  const name = "Jade Isip";
+  const aboutMe = "About Me";
+  const aboutInfo =
+    "I am an aspiring software engineer from the Land of the Pearl of the Orients. My end goal is to become a cloud engineer but I also took interest in web development, hence why I am in the TOD curriculum. If you want to find a coding buddy or you need someone for backend development, primarily something to do with the cloud, then do not hesitate to reach out to me!";
 
-  const skills = 'My target languages';
+  const skills = "My target languages";
 
-  const contactMe = 'Contact Me';
-  const contactMsg = "Don't hesitate to reach out if you think we share my interest benefits yours. Happy Coding!";
-  const contactNo = '+63 987 6543 321';
-  const email = 'jademattieuisip@gmail.com';
+  const contactMe = "Contact Me";
+  const contactMsg =
+    "Don't hesitate to reach out if you think we share interest or it benefits yours. Happy Coding!";
+  const contactNo = "+63 987 6543 321";
+  const email = "jademattieuisip@gmail.com";
 
-  const nameHeader = document.querySelector('.about-picture > h1');
-  const aboutHeader = document.querySelector('.about-me > h1');
-  const aboutP = document.querySelector('.about-me > p');
+  const nameHeader = document.querySelector(".about-picture > h1");
+  const aboutHeader = document.querySelector(".about-me > h1");
+  const aboutP = document.querySelector(".about-me > p");
 
-  const mainHeader = document.querySelector('main > h1');
+  const mainHeader = document.querySelector("main > h1");
 
-  const footerHeader = document.querySelector('footer > div > h1');
-  const footerMessage = document.querySelector('footer > div > p');
-  const callP = document.querySelector('.phone > p');
-  const mailP = document.querySelector('.mail > p');
+  const footerHeader = document.querySelector("footer > div > h1");
+  const footerMessage = document.querySelector("footer > div > p");
+  const callP = document.querySelector(".phone > p");
+  const mailP = document.querySelector(".mail > p");
 
   nameHeader.textContent = name;
   aboutHeader.textContent = aboutMe;
@@ -40,31 +42,22 @@ import linkedinIcon from './assets/linkedin.svg';
 })();
 
 (function addIcons() {
-  const linkContainers = document.querySelectorAll('.links');
-  linkContainers.forEach(container => {
-    const github = document.createElement('img');
-    const linkedIn = document.createElement('img');
+  const linkContainers = document.querySelectorAll(".links");
+  linkContainers.forEach((container) => {
+    const github = document.createElement("img");
+    const linkedIn = document.createElement("img");
     const twitter = document.createElement("img");
 
     github.src = githubIcon;
     linkedIn.src = linkedinIcon;
     twitter.src = twitterIcon;
 
-    github.alt = '';
-    linkedIn.alt = '';
-    twitter.alt = '';
+    github.alt = "";
+    linkedIn.alt = "";
+    twitter.alt = "";
 
     container.append(github, linkedIn, twitter);
-  })
+  });
 })();
 
 
-
-
-
-// My target skils
-
-// If you're a fellow beginner and want a coding buddy, feel free to
-//           contact me through links below.See ya!
-          
-// Contact Me
