587bafde0152668aa47420c71e24a6a1e7603454
jade <121040221+stephenstrange-blip@users.noreply.github.com>
2025-04-03 15:19:08 +0800

    Refactor: Add contant via js

--- src/index.js
+++ src/index.js
@@ -1,2 +1,70 @@
 import "./css_reset.css";
-import "./styles.css";
+import "./styles.css";document.createElement('img');
+import githubIcon from './assets/github.svg';
+import twitterIcon from './assets/twitter.svg';
+import linkedinIcon from './assets/linkedin.svg';
+
+
+
+(function addText() {
+  const name = 'Jade Isip';
+  const aboutMe = 'About Me';
+  const aboutInfo = 'I am an aspiring software engineer from the Land of the Pearl of the Orients. My end goal is to become a cloud engineer but I also took interest in web development, hence why I am in the TOD curriculum. If you want to find a coding buddy or you need someone for backend development, primarily something to do with the cloud, then do not hesitate to reach out to me!';
+
+  const skills = 'My target languages';
+
+  const contactMe = 'Contact Me';
+  const contactMsg = "Don't hesitate to reach out if you think we share my interest benefits yours. Happy Coding!";
+  const contactNo = '+63 987 6543 321';
+  const email = 'jademattieuisip@gmail.com';
+
+  const nameHeader = document.querySelector('.about-picture > h1');
+  const aboutHeader = document.querySelector('.about-me > h1');
+  const aboutP = document.querySelector('.about-me > p');
+
+  const mainHeader = document.querySelector('main > h1');
+
+  const footerHeader = document.querySelector('footer > div > h1');
+  const footerMessage = document.querySelector('footer > div > p');
+  const callP = document.querySelector('.phone > p');
+  const mailP = document.querySelector('.mail > p');
+
+  nameHeader.textContent = name;
+  aboutHeader.textContent = aboutMe;
+  aboutP.textContent = aboutInfo;
+  mainHeader.textContent = skills;
+  footerHeader.textContent = contactMe;
+  footerMessage.textContent = contactMsg;
+  callP.textContent = contactNo;
+  mailP.textContent = email;
+})();
+
+(function addIcons() {
+  const linkContainers = document.querySelectorAll('.links');
+  linkContainers.forEach(container => {
+    const github = document.createElement('img');
+    const linkedIn = document.createElement('img');
+    const twitter = document.createElement("img");
+
+    github.src = githubIcon;
+    linkedIn.src = linkedinIcon;
+    twitter.src = twitterIcon;
+
+    github.alt = '';
+    linkedIn.alt = '';
+    twitter.alt = '';
+
+    container.append(github, linkedIn, twitter);
+  })
+})();
+
+
+
+
+
+// My target skils
+
+// If you're a fellow beginner and want a coding buddy, feel free to
+//           contact me through links below.See ya!
+          
+// Contact Me
